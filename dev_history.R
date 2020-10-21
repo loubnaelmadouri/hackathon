4ebb92fd41c74fc0296db0102bbd243ebf940208
loubnaelmadouri <you@example.com>
2020-10-21 14:50:37 +0200

    :tada:create package homepage

--- dev_history.R
+++ dev_history.R
@@ -15,6 +15,9 @@ usethis::use_build_ignore("README.html")
 usethis::use_git_ignore("README.html")
 usethis::use_build_ignore("README.md")
 
+#crée une fonction qui ne sert pas mais fait l'accueil du site web
+usethis::use_r("hackathon-package")
+
 usethis::use_r("makeTop")
 
 #Génère la documentation
@@ -23,7 +26,11 @@ attachment::att_amend_desc() #ajoute les packages dans la description
 
 devtools::check()
 
+devtools::install()
+
 usethis::use_readme_rmd()
 
 #Générer le mark down chaque fois que le readme est édité
 rmarkdown::render("README.Rmd")
+
+
